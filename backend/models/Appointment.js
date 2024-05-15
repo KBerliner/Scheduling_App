fcc4eba33a2ee810d0b308981d2c87afded1ea59
Kyle <berlinerkyle@gmail.com>
2024-05-15 16:38:36 -0600

    Added the Appointment model.

---
+++ backend/models/Appointment.js
@@ -0,0 +1,21 @@
+// Installing Dependencies
+
+const mongoose = require("mongoose");
+
+// Creating a Blueprint for the "Appointment" Object
+
+const appointmentSchema = mongoose.Schema({
+	customerName: { type: String, required: true },
+	customerEmail: { type: String, required: true },
+	customerPhone: { type: String, required: true },
+	appointmentDate: { type: Date, required: true },
+	appointmentStartTime: { type: String, required: true },
+	appointmentEndTime: { type: String, required: true },
+	appointmentNotes: { type: String, required: true },
+	createdAt: { type: Date, default: new Date() },
+	updatedAt: { type: Date, default: new Date() },
+});
+
+// Exporting the Appointment Blueprint
+
+module.exports = mongoose.model("Appointment", appointmentSchema);
